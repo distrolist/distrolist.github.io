b5ae884c84189baaf7e6e749208f4c1c9864ff77
Ali Rıza KESKİN <parduscix@yandex.ru>
2021-05-17 22:13:14 +0300

    lists

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 import urllib
 sources=[]
 def get_sources(link=""):
+    print("Import: "+str(link))
     list=urllib.urlopen(link).read()
     for line in list.split("\n"):
-        if "import " in line:
+        if len(line)<5:
+            pass
+        elif "import " in line:
             get_sources(line.split(" ")[1])
-        else:
+        elif line[0] != "#":
+            if "gh://" in line:
+                line.replace("gh://","https://raw.githubusercontent.com/")
             sources.append(line)
 
 get_sources("https://raw.githubusercontent.com/paledega/distrolist.github.io/master/main.list")
+get_sources("import https://raw.githubusercontent.com/paledega/distrolist.github.io/master/imports.list")
 print(sources)
