4779bc42b3d8cfc955153f9f8b7a7403872637b9
Muhammed Efe Çetin <efectn@protonmail.com>
2021-05-20 21:37:00 +0300

    Distro parser

--- site-builder/build.py
+++ site-builder/build.py
@@ -1,10 +1,21 @@
 #!/usr/bin/python3
 import os
+import yaml
+import glob
+from rst2html import rst2html
 from template import page
+
 os.chdir("..")
-sources=os.listdir("build/sources")
+sources = os.listdir("build/sources")
+
 for s in sources:
-    p=page("site-builder/template.html")
-    p.set_var("content","RST2HTML body")
-    p.set_var("logo","https://raw.githubusercontent.com/distrolist/data/master/res/debian-logo.svg")
+    contents = yaml.load(open(glob.glob("build/sources/" + s + "/yaml/*.yaml")[0]).read(),
+                         Loader=yaml.FullLoader)
+
+    with open("build/sources/main/rst/" + contents["description"], "r") as f:
+        description, warning = rst2html(f.read())
+
+    p = page("site-builder/template.html")
+    p.set_var("content", description)
+    p.set_var("logo", "https://raw.githubusercontent.com/distrolist/data/master/res/" + contents["logo"])
     p.save()
