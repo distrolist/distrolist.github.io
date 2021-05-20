7e8b24059a9cb6f61a69b52a2a7cf4f46a06e055
sulincix <55052890+sulincix@users.noreply.github.com>
2021-05-20 19:37:12 +0300

    Update template.py

--- site-builder/template.py
+++ site-builder/template.py
@@ -5,6 +5,7 @@ class page:
         self.name="example"
         self.css=["../site-builder/main.css"]
     def save(self):
+        """save template to file"""
         f=open("{}/{}.html".format(self.path,self.name),"w")
         f.write("<html>\n  <head>\n")
         for c in self.css:
@@ -15,4 +16,5 @@ class page:
         f.write("  </body>\n</html>")
         f.close()
     def set_var(self,variable,value):
+        """set variable from template set_var("name","example")"""
         self.template=self.template.replace("@{}@".format(variable),value)
